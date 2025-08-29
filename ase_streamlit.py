e490f89680040ec5834ded9c77480c3ae4fdbef6
Salman <salmanshahidzia@gmail.com>
2025-08-29 21:19:06 +0000

    fix viewer

--- ase_streamlit.py
+++ ase_streamlit.py
@@ -3,9 +3,16 @@ from ase import io
 import py3Dmol
 from stmol import showmol
 
+# Format formula with subscripts using st.markdown and LaTeX
+def formula_to_latex(formula):
+    import re
+    # Replace numbers with LaTeX subscripts
+    return re.sub(r'(\d+)', r'$_{\1}$', formula)
+
 st.title("ASE Visualizer")
 
 uploaded_file = st.file_uploader("Upload a file", type=["cif", "pdb", "sdf", "xyz"])
+
 
 if uploaded_file:
     atoms = io.read(uploaded_file)
@@ -16,10 +23,12 @@ if uploaded_file:
     for atom, pos in zip(atoms, atoms.positions):
         xyz_str += f"{atom.symbol} {pos[0]} {pos[1]} {pos[2]}\n"
 
-    # Display with py3Dmol
+    st.title(formula_to_latex(atoms.get_chemical_formula()))
+    st.write("Cell parameters:", atoms.cell.lengths().tolist())
+    st.write("Number of atoms:", len(atoms))
+
     viewer = py3Dmol.view(width=500, height=500)
     viewer.addModel(xyz_str, "xyz")
     viewer.setStyle({"stick":{}})
     viewer.zoomTo()
     showmol(viewer)
-
